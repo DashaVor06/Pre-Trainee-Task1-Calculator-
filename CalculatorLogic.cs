b47c45f478ce060da5960952a4c06b00cc4900d5
DashaVor06 <dashasolnyshko.ds@gmail.com>
2025-10-03 19:37:59 +0300

    add class with calculator logic, correct mistakes

---
+++ CalculatorLogic.cs
@@ -0,0 +1,77 @@
+﻿namespace Task1_Calculator
+{
+    public class CalculatorLogic
+    {
+        private List<char> _operations = new List<char>() {'+', '-', '*', '/'};
+        public int GetNumberOrExit(string text, bool fSearchExit, out bool fEnteredExit)
+        {
+            int num = 0, res;
+            fEnteredExit = false;
+            do
+            {
+                Console.WriteLine(text);
+                string? strNum = Console.ReadLine();
+
+                if (fSearchExit && strNum == "exit")
+                {
+                    fEnteredExit = true;
+                    break;
+                }
+
+                if (int.TryParse(strNum, out res))
+                {
+                    num = res;
+                    break;
+                }
+
+            } while (true);
+
+            return num;
+        }
+
+        char? GetOperationFromString(string? strOper)
+        {
+            if (strOper != null && strOper != "" && _operations.Contains(strOper[0]))
+                return strOper[0];
+            return null;
+        }
+
+        public char GetOperation(string text)
+        {
+            char? operation;
+            do
+            {
+                Console.WriteLine(text);
+                string? strOperation = Console.ReadLine();
+                operation = GetOperationFromString(strOperation);
+            } while (operation == null);
+
+            return (char)operation;
+        }
+
+        public int Calculate(int num1, char oper, int num2, out bool fDevideByZero)
+        {
+            int result = 0;
+            fDevideByZero = false;
+            switch (oper)
+            {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    if (num2 == 0)
+                        fDevideByZero = true;
+                    else
+                        result = num1 / num2;
+                    break;
+            }
+            return result;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,102 +1,22 @@
-﻿int? getNumFromStr(string? strNum)
+﻿using Task1_Calculator;
+
+var calc = new CalculatorLogic();
+
+bool fSearchExit = true, fEnteredExit = false, fDevideByZero = false;
+
+while (true)
 {
-    if (strNum == null || strNum == "")
-        return null;
-    foreach (char symbol in strNum)
-    {
-        if (symbol < '0' || symbol > '9')
-            return null;
-    }
-    return int.Parse(strNum);
-}
-
-int getNum(string text)
-{
-    int? num;
-    do
-    {
-        Console.WriteLine(text);
-        string? strNum = Console.ReadLine();
-        num = getNumFromStr(strNum);
-    } while (num == null);
-
-    return (int)num;
-}
-
-int? getNumOrExit(string text)
-{
-    int? num;
-    do
-    {
-        Console.WriteLine(text);
-        string? strNum = Console.ReadLine();
-        if (strNum == "exit")
-            return null;
-        num = getNumFromStr(strNum);
-    } while (num == null);
-
-    return num;
-}
-
-char? getOperFromStr(string? strOper)
-{
-    if (strOper != null && (strOper[0] == '+' || strOper[0] == '-' || strOper[0] == '*' || strOper[0] == '/'))
-        return strOper[0];
-    return null;
-}
-
-char getOper(string text)
-{
-    char? operation;
-    do
-    {
-        Console.WriteLine(text);
-        string? strOperation = Console.ReadLine();
-        operation = getOperFromStr(strOperation);
-    } while (operation == null);
-
-    return (char)operation;
-}
-
-int? calculate(int num1, char oper, int num2)
-{
-    int? result = 0;
-    switch (oper) {
-        case '+':
-            result = num1 + num2;
-            break;
-        case '-':
-            result = num1 - num2;
-            break;
-        case '*': 
-            result = num1 * num2;
-            break;
-        case '/':
-            if (num2 == 0)
-            {
-                Console.WriteLine("Devide by zero exeption");
-                result = null;
-            }
-            else
-                result = num1 / num2;
-            break;
-    }
-    return result;
-}
-
-bool fExit = false;
-while (!fExit)
-{
-    int? num1 = getNumOrExit("Enter first number or command 'exit': ");
-    if (num1 == null)
-    {
-        fExit = true;
+    int num1 = calc.GetNumberOrExit("Enter first number or command 'exit': ", fSearchExit, out fEnteredExit);
+    if (fEnteredExit) 
         break;
-    }
-    char oper = getOper("Enter operation: ");
-    int num2 = getNum("Enter second number: ");
 
-    int? result = calculate((int)num1, oper, num2);
-    if (result != null)
+    char oper = calc.GetOperation("Enter operation: ");
+
+    int num2 = calc.GetNumberOrExit("Enter second number: ", !fSearchExit, out fEnteredExit);
+
+    int result = calc.Calculate(num1, oper, num2, out fDevideByZero);
+    if (!fDevideByZero)
         Console.WriteLine("Result of operation: " + result);
+    else
+        Console.WriteLine("Devide by zero exeption");
 }
